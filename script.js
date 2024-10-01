2d356e962f753f073cca7795accb606b6ab5716f
duKraut <edukrautchuk@gmail.com>
2024-10-01 20:32:07 -0300

    feat: exercício 3

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-//questão 2
+//questão 3
 
 let numero = ("Qual é o número? ");
 numero = Number(numero);
 
-if (numero % 2 === 0) {
-    console.log("Você é par")
+if (numero < 0) {
+    console.log("Você é negativo")
+
+} if (numero = 0) {
+    console.log("Você é igual a zero")
+
 } else {
-    console.log("Você é impar")
+    console.log("Você é positivo")
 }
