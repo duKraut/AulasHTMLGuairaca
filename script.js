52a360a97086ed4bf61db8f5dbc20183caa3892f
duKraut <edukrautchuk@gmail.com>
2024-10-22 20:22:47 -0300

    feat: animações

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-document.getElementById("contact-form").addEventListener(
-    "submit", function(event) {
-        event.preventDefault();
-        console.log(event);
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
+let rotationDegree = 0;
 
-        const mailtolink = 
-        `mailto:${email}?
-        subject=${encodeURIComponent('Assunto Reunião')}
-        &body=${encodeURIComponent(message)}`;
-        
-        window.location.href = mailtolink;
-    }
-);
+function rotateElement() {
+    rotationDegree +=45;
+    document.getElementById('rotation-div').style
+    .transform = `rotate(${rotationDegree}deg)`;
+}
+
+function toggleFade() {
+    const div = document.getElementById('fading-div');
+    div.classList.toggle('visible');
+}
+
+function togglePulse() {
+    const div = document.getElementById('pulsing-div');
+    div.classList.toggle('pulsing');
+}
+
+function zoomIn() {
+    const div = document.getElementById('zooming-div');
+    div.classList.add('zooming');
+}
